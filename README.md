95a93d07e082b50873b88f6cdfc7bfadad93feb5
chenyuhao <cccchenyuhao@gmail.com>
2021-10-08 12:02:00 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # nasa-mission-control
+
+```bash
+mkdir client server
+
+cd client
+npx create-react-app .
+npm install arwes
+
+npm run start
+```
+
+
+
---
+++ client/src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom'
+
+const element = <h1>hello world</h1>
+
+const root = document.getElementById('root')
+
+ReactDOM.render(element, root)
