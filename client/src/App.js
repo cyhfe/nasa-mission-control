d51e51e2cfec17498bd2636e5205903144a87d8c
chenyuhao <cccchenyuhao@gmail.com>
2021-10-25 23:53:35 +0800

    client

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ThemeProvider, createTheme, Arwes, Button } from 'arwes';
+
 export function App() {
-  return <div>app</div>;
+  return (
+    <ThemeProvider theme={createTheme()}>
+      <Arwes>
+        <h1>My App</h1>
+        <p>A SciFi Project</p>
+        <div style={{ padding: 20 }}>
+          <Button>My Button</Button>
+        </div>
+      </Arwes>
+    </ThemeProvider>
+  );
 }
