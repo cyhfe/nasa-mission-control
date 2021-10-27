475074f300510f145368a26cf26233fed2b98b2f
chenyuhao <cccchenyuhao@gmail.com>
2021-10-27 23:47:40 +0800

    update

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@ export function App() {
   return (
     <ThemeProvider theme={createTheme()}>
       <Arwes>
-        <h1>My App</h1>
         <p>A SciFi Project</p>
         <div style={{ padding: 20 }}>
           <Button>My Button</Button>
